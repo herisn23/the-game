76ce238f0711ed8c269f0d036f32d0eab8b300a5
lukastreml <work@treml.cz>
2026-01-10 14:47:39 +0100

    chore: add new assets

--- game/layer/gameplay/scene/src/main/kotlin/org/roldy/gameplay/scene/GameScene.kt
+++ game/layer/gameplay/scene/src/main/kotlin/org/roldy/gameplay/scene/GameScene.kt
@@ -3,14 +3,10 @@ package org.roldy.gameplay.scene
 import org.roldy.core.CursorManager
 import org.roldy.core.Logger
 import org.roldy.core.TimeManager
-import org.roldy.core.asset.AtlasLoader
 import org.roldy.core.coroutines.DeltaProcessingLoop
-import org.roldy.data.mine.HarvestableType
-import org.roldy.gameplay.scene.initializers.loadingScreen
-import org.roldy.gameplay.scene.initializers.worldScreen
-import org.roldy.gui.CraftingIconTextures
-import org.roldy.gui.CraftingIconTexturesType
+import org.roldy.gameplay.scene.dev.TestTiledMapBlendScreen
 import org.roldy.rendering.g2d.disposable.AutoDisposableGameAdapter
+import org.roldy.rendering.screen.ProxyScreen
 
 class GameScene : AutoDisposableGameAdapter() {
 
@@ -25,19 +21,14 @@ class GameScene : AutoDisposableGameAdapter() {
     override fun create() {
         Logger.level = Logger.Level.Debug
         CursorManager.initialize()
-        val atlas = AtlasLoader.craftingIcons
-        val craftingTextures = CraftingIconTextures(atlas)
-        HarvestableType.harvestable.forEach {
-            craftingTextures.region(it, CraftingIconTexturesType.Background)
-            craftingTextures.region(it, CraftingIconTexturesType.Normal)
-        }
-//        val camera = camera(10f)
+        val camera = camera(30f)
+        screen = ProxyScreen(TestTiledMapBlendScreen(camera), camera)
 //        screen = ProxyScreen(DebugTiledMapScreen(camera), camera)
 
 
-        loadingScreen { setProgress ->
-            worldScreen(setProgress)
-        }
+//        loadingScreen { setProgress ->
+//            worldScreen(setProgress)
+//        }
 
     }
 
